ed395517abc5e6e549bb77f2b18404c9418b3998
Jose Romulo Guzman <84124290+DaUndeadGuy@users.noreply.github.com>
2023-03-27 19:50:52 +0800

    Reworked fracture and bullet
    Red Cube is still WIP

--- Source/UnrealChallenge02/UnrealChallenge02Projectile.cpp
+++ Source/UnrealChallenge02/UnrealChallenge02Projectile.cpp
@@ -33,7 +33,6 @@ AUnrealChallenge02Projectile::AUnrealChallenge02Projectile()
 
 void AUnrealChallenge02Projectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
 {
-	// Only add impulse and destroy projectile if we hit a physics
 	if ((OtherActor != nullptr) && (OtherActor != this) && (OtherComp != nullptr) && OtherComp->IsSimulatingPhysics())
 	{
 		OtherComp->AddImpulseAtLocation(GetVelocity() * 100.0f, GetActorLocation());
